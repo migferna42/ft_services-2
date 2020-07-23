b8d656789694e9e85e007be2eb80cdf31d582703
Matthias BRUNEL <mbrunel@e3r9hsu.fr>
2020-07-23 05:13:27 +0200

    one could say it's getting better

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # install docker and minikube if necessary
+brew uninstall -f docker docker-compose docker-machine || true
 if ! which docker >/dev/null 2>&1 ||
     ! which minikube >/dev/null 2>&1
 then
@@ -8,32 +9,52 @@ then
     exit 1
 fi
 
+docker_destination="/goinfre/$USER/docker"
+minikube_destination="/goinfre/$USER/minikube"
+
+# Create needed files in destination and make symlinks
+if [ ! -d $docker_destination ]; then
+	pkill Docker
+	rm -rf ~/Library/Containers/com.docker.docker ~/.docker
+	mkdir -p $docker_destination/{com.docker.docker,.docker}
+	ln -sf $docker_destination/com.docker.docker ~/Library/Containers/com.docker.docker
+	ln -sf $docker_destination/.docker ~/.docker
+fi
+
+# Start Docker for Mac
+open -a Docker
+
+
+export MINIKUBE_HOME=$minikube_destination
+
 # Ensure minikube is launched
 if ! minikube status >/dev/null 2>&1
 then
     echo Minikube is not started! Starting now...
-    if ! minikube start --vm-driver=docker \
+    if ! minikube start --vm-driver=virtualbox \
         --bootstrapper=kubeadm # allow telegraf to query metrics
     then
         echo Cannot start minikube!
         exit 1
     fi
     minikube addons enable metrics-server
+	kubectl edit configmap -n kube-system kube-proxy
+	#kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+	#kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+	#kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 fi
 
-
 eval $(minikube docker-env)
-
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+#kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+#kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
 docker build -t nginx-image nginx
-kubectl apply -f nginx.yaml 
+kubectl apply -f nginx.yaml
+kubectl apply -f loadbalancer.yaml
 
-minikube service nginx-service
-
-echo -n "Press a key to continue"
+minikube service loadbalancer
+echo "\nPress a key to continue"
 read hop
-kubectl delete service nginx-service 
-kubectl delete service loadbalancer
+
+kubectl delete service nginx-service
 kubectl delete deployment nginx-deployment
-kubectl delete deployment -n metallb-system controller
+kubectl delete service loadbalancer
